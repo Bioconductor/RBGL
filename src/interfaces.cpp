fc9affb47ac0b6ef7eabde0422b299c0459bf245
Vince Carey <stvjc@channing.harvard.edu>
2003-07-13 12:41:16 +0000

    ok
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RBGL@5128 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/interfaces.cpp
+++ src/interfaces.cpp
@@ -8,41 +8,66 @@ extern "C" {
 
 extern "C" 
 {
-	SEXP BGL_tsort_D(SEXP num_verts_in, SEXP num_edges_in, SEXP R_edges_in)
-	{
-	int NV = INTEGER(num_verts_in)[0];
-	setupGraphTypes
-	setTraits( Graph_dd )
-	setUnweightedDoubleEdges( Graph_dd )
-	   
-	typedef list<Vertex> tsOrder;
-	tsOrder tsord;
-	SEXP tsout;
-	
-	PROTECT(tsout = NEW_NUMERIC(NV));
-	
-	try {
-	      topological_sort(g, std::front_inserter(tsord));
-	
-	      int j = 0;
-	      for (tsOrder::iterator i = tsord.begin();
-	             i != tsord.end(); ++i)
-	             {
-	             REAL(tsout)[j] = (double) *i;
-	             j++;
-	             }
-	      }
-	catch ( not_a_dag )
-	      {
-	      Rprintf("not a dag, returning zeroes\n");
-	      for (int j = 0 ; j < NV; j++)
-	          REAL(tsout)[j] = 0.0;
-	      }
-	UNPROTECT(1);
-	
-	return(tsout);
-	      
-	} // end BGLtsortD
+
+
+SEXP BGL_tsort_D(SEXP num_verts_in, SEXP num_edges_in, SEXP R_edges_in)
+{
+// tsortbCG -- for bioConductor graph objects (only graphNEL at present)
+  
+Rprintf("pre ty\n");
+setupGraphTypes
+Rprintf("pre tra\n");
+setTraits( Graph_dd )
+Rprintf("pre ed\n");
+setUnweightedDoubleEdges( Graph_dd )
+
+Rprintf("pre color\n");
+  typedef property_map<Graph_dd, vertex_color_t>::type Color;
+Rprintf("pre it\n");
+  graph_traits<Graph_dd>::vertex_iterator viter, viter_end;
+
+ 
+  typedef list<Vertex> tsOrder;
+  tsOrder tsord;
+  SEXP tsout;
+
+Rprintf("pre tsout\n");
+  PROTECT(tsout = NEW_NUMERIC(INTEGER(num_verts_in)[0]));
+
+  try {
+Rprintf("pre ts\n");
+      topological_sort(g, std::front_inserter(tsord));
+Rprintf("post ts\n");
+
+      int j = 0;
+      for (tsOrder::iterator i = tsord.begin();
+             i != tsord.end(); ++i)
+             {
+             REAL(tsout)[j] = (double) *i;
+             j++;
+             }
+      }
+  catch ( not_a_dag )
+      {
+      Rprintf("not a dag, returning zeroes\n");
+      for (int j = 0 ; j < INTEGER(num_verts_in)[0]; j++)
+          REAL(tsout)[j] = 0.0;
+      }
+  UNPROTECT(1);
+
+  return(tsout);
+      
+  } // end BGL_tsort_D
+ 
+/*
+main() {
+int nv = 15, ne = 19;
+double x[] = {
+ 0. , 4. , 0. , 6. , 0. , 1. , 1. , 6. , 1. , 11. , 2. , 6. , 2. , 9. , 2. , 11. , 3. , 4. , 4. , 5. , 5. , 8. , 6. , 7. , 7. ,
+ 8. , 8. , 13. , 9. , 10. , 10. , 13. , 11. , 12. , 12. , 13. , 13. , 14. };
+tsortSG( &nv, &ne, x , x );
+}
+*/
 
 
 	SEXP BGL_KMST_D( SEXP num_verts_in, SEXP num_edges_in, 
@@ -83,5 +108,5 @@ extern "C"
 	SET_VECTOR_ELT(ansList,1,answt);
 	UNPROTECT(3);
 	return(ansList);
-	}
+	} //end BGL_KMST_D
 }
