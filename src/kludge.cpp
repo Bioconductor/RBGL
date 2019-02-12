1d76c84a2340f0b05948e0e0e64c51f5b2941e67
Samuela Pollack <spollack@jimmy.harvard.edu>
2019-02-12 18:55:20 -0500

    Fixed error in error message

--- src/kludge.cpp
+++ src/kludge.cpp
@@ -5,7 +5,7 @@ extern "C"  {
 void _bgl_abort()
 {
     Rf_error
-        ("internal: samtools invoked 'abort'; see warnings() and restart R");
+        ("internal: RBGL invoked 'abort'; see warnings() and restart R");
 }
 }
 
